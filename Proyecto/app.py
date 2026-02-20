341a1953c81b985d9b5acec220744f9fdff7727b
Lucas-Ramon <ra.lucasc@uea.edu.ec>
2026-02-20 09:55:49 -0500

    .
    .

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/producto/<nombre>')
-def producto(nombre):
-    return f"Producto: {nombre} – disponible."
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/factura')
+def factura():
+    return render_template('factura.html')
+
+@app.route('/contactos')
+def contactos():
+    return render_template('contactos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
